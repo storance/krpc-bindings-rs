bd6b4fe4082d4e4cd356ef37b5e21aa51ffe08ea
Steven Torance <storance@gmail.com>
2018-12-29 00:41:12 -0500

    Add doc for ReferenceFrame.

--- src/spacecenter/reference_frame.rs
+++ src/spacecenter/reference_frame.rs
@@ -4,10 +4,40 @@ use crate::codec::*;
 use std::rc::{Rc};
 use std::cell::{RefCell};
 
-remote_type!(object ReferenceFrame {});
+remote_type!(
+/// Represents a reference frame for positions, rotations and velocities. Contains:
+///
+/// * The position of the origin.
+/// * The directions of the x, y and z axes.
+/// * The linear velocity of the frame.
+/// * The angular velocity of the frame.
+///
+/// # Note
+/// This struct does not contain any class methods. It is only to be used as a parameter to
+/// other functions.
+object ReferenceFrame {});
 
 impl ReferenceFrame {
-    rpc_method!(fn create_relative(client: &KrpcClient,
+    rpc_method!(
+    /// Create a relative reference frame. This is a custom reference frame whose components
+    /// offset the components of a parent reference frame.
+    ///
+    /// **Game Scenes***: All
+    ///
+    /// # Arguments
+    /// * `client` - The krpc client.
+    /// * `reference_frame` - The parent reference frame on which to base this reference frame.
+    /// * `position` - The offset of the position of the origin, as a position vector. If `None`,
+    /// (0,0,0) is used.
+    /// * `rotation` - The rotation to apply to the parent frames rotation, as a quaternion of the
+    /// form (x,y,z,w). If `None`, (0,0,0,1) is used (i.e. no rotation)
+    /// * `velocity` - The linear velocity to offset the parent frame by, as a vector pointing in
+    /// the direction of travel, whose magnitude is the speed in meters per second. If `None`,
+    /// (0,0,0) is used.
+    /// * `angular_velocity` - The angular velocity to offset the parent frame by, as a vector.
+    /// This vector points in the direction of the axis of rotation, and its magnitude is the
+    /// speed of the rotation in radians per second. If `None`, (0,0,0) is used.
+    fn create_relative(client: &KrpcClient,
                            reference_frame: &ReferenceFrame,
                            position: &Option<Vector3>,
                            rotation: &Option<Quaternion>,
@@ -25,7 +55,23 @@ impl ReferenceFrame {
         }
     });
 
-    rpc_method!(fn create_hybrid(client: &KrpcClient,
+    rpc_method!(
+    /// Create a hybrid reference frame. This is a custom reference frame whose components
+    /// inherited from other reference frames.
+    ///
+    /// **Game Scenes***: All
+    ///
+    /// # Arguments
+    /// * `client` - The krpc client.
+    /// * `reference_frame` - The parent reference frame on which to base this reference frame.
+    /// * `position` - The reference frame providing the position of the origin.
+    /// * `rotation` - The reference frame providing the rotation of the frame. If `None`,
+    /// the rotation of the `position` frame is used.
+    /// * `velocity` - The reference frame providing the linear velocity of the frame. If `None`,
+    /// the linear velocity of the `position` frame is used.
+    /// * `angular_velocity` - The reference frame providing the angular velocity of the frame.
+    /// If `None`, the angular velocity of the `position` frame is used.
+    fn create_hybrid(client: &KrpcClient,
             position: &ReferenceFrame,
             rotation: &Option<ReferenceFrame>,
             velocity: &Option<ReferenceFrame>,
