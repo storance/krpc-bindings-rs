1d16c624ab1a8c92d6d309a0198c102df41882fc
Steven Torance <storance@gmail.com>
2019-01-05 19:14:42 -0500

    Implement resources

--- src/spacecenter/resources.rs
+++ src/spacecenter/resources.rs
@@ -1,5 +1,6 @@
 use crate::*;
 use crate::codec::*;
+use super::{Part};
 
 use std::rc::{Rc};
 use std::cell::{RefCell};
@@ -8,17 +9,213 @@ remote_type!(
     /// Represents the collection of resources stored in a vessel, stage or part. Created by
     /// calling `Vessel::resources()`, `Vessel::resources_in_decouple_stage()` or
     /// `Part::resources()`.
-    object Resources {}
+    object Resources {
+        service: SpaceCenter,
+        properties: {
+            {
+                All: Vec<Resource>,
+                /// Returns all the individual resources that can be stored.
+                ///
+                /// **Game Scenes**: Flight
+                get: all
+            }
+            {
+                Names: Vec<String>,
+                /// Returns a list of resource names that can be stored.
+                ///
+                /// **Game Scenes**: Flight
+                get: names
+            }
+            {
+                Enabled: bool,
+                /// Returns whether use of all the resources are enabled.
+                ///
+                /// **Game Scenes**: Flight
+                get: is_enabled,
+                /// Sets whether use of all the resources are enabled.
+                ///
+                /// **Game Scenes**: Flight
+                set: set_enabled
+            }
+        },
+        methods: {
+            {
+                /// Returns all the individual resources with the given name that can be stored.
+                ///
+                /// **Game Scenes**: Flight
+                ///
+                /// # Arguments
+                /// * `name` - The name of the resource.
+                fn with_resource(name: String) -> Vec<Resource> {
+                    WithResource(name)
+                }
+            }
+            {
+                /// Check whether the named resource can be stored.
+                ///
+                /// **Game Scenes**: Flight
+                ///
+                /// # Arguments
+                /// * `name` - The name of the resource.
+                fn has_resource(name: String) -> bool {
+                    HasResource(name)
+                }
+            }
+            {
+                /// Returns the amount of a resource that is currently stored.
+                ///
+                /// **Game Scenes**: Flight
+                ///
+                /// # Arguments
+                /// * `name` - The name of the resource.
+                fn amount(name: String) -> f32 {
+                    Amount(name)
+                }
+            }
+            {
+                /// Returns the amount of a resource that can be stored.
+                ///
+                /// **Game Scenes**: Flight
+                ///
+                /// # Arguments
+                /// * `name` - The name of the resource.
+                fn max(name: String) -> f32 {
+                    Max(name)
+                }
+            }
+        },
+        static_methods: {
+            {
+                /// Returns the density of a resource, in kg/l.
+                ///
+                /// **Game Scenes**: Flight
+                ///
+                /// # Arguments
+                /// * `name` - The name of the resource.
+                fn density(name: String) -> f32 {
+                    Density(name)
+                }
+            }
+            {
+                /// Returns the flow mode of a resource.
+                ///
+                /// **Game Scenes**: Flight
+                ///
+                /// # Arguments
+                /// * `name` - The name of the resource.
+                fn flow_mode(name: String) -> ResourceFlowMode {
+                    FlowMode(name)
+                }
+            }
+        }
+    }
 );
 
 remote_type!(
     /// An individual resource stored within a part. Created using methods in the Resources class.
-    object Resource {}
+    object Resource {
+        service: SpaceCenter,
+        properties: {
+            {
+                Name: String,
+                /// Returns the name of the resource.
+                ///
+                /// **Game Scenes**: All
+                get: name
+            }
+            {
+                Part: Part,
+                /// Returns the part containing the resource.
+                ///
+                /// **Game Scenes**: All
+                get: part
+            }
+            {
+                Amount: f32,
+                /// Returns the amount of the resource that is currently stored in the part.
+                ///
+                /// **Game Scenes**: All
+                get: amount
+            }
+            {
+                Max: f32,
+                /// Returns the total amount of the resource that can be stored in the part.
+                ///
+                /// **Game Scenes**: All
+                get: max
+            }
+            {
+                Density: f32,
+                /// Returns the density of the resource, in kg/l.
+                ///
+                /// **Game Scenes**: All
+                get: density
+            }
+            {
+                FlowMode: ResourceFlowMode,
+                /// Returns the flow mode of the resource.
+                ///
+                /// **Game Scenes**: All
+                get: flow_mode
+            }
+            {
+                Enabled: bool,
+                /// Returns whether use of this resource is enabled.
+                ///
+                /// **Game Scenes**: All
+                get: is_enabled,
+                /// Sets whether use of this resource is enabled.
+                ///
+                /// **Game Scenes**: All
+                set: set_enabled
+            }
+        }
+    }
 );
 
 remote_type!(
     /// Transfer resources between parts.
-    object ResourceTransfer {}
+    object ResourceTransfer {
+        service: SpaceCenter,
+        properties: {
+            {
+                Amount: f32,
+                /// Returns the amount of the resource that has been transferred.
+                ///
+                /// **Game Scenes**: All
+                get: amount
+            }
+            {
+                Complete: bool,
+                /// Returns whether the transfer has completed.
+                ///
+                /// **Game Scenes**: All
+                get: complete
+            }
+        },
+        methods: {},
+        static_methods: {
+            {
+                /// Start transferring a resource transfer between a pair of parts.
+                /// The transfer will move at most `max_amount` units of the resource, depending
+                /// on how much of the resource is available in the source part and how much
+                /// storage is available in the destination part. Use `ResourceTransfer::complete()`
+                /// to check if the transfer is complete. Use `ResourceTransfer::amount()`
+                /// to see how much of the resource has been transferred.
+                ///
+                /// **Game Scenes**: All
+                ///
+                /// # Arguments
+                /// * `from_part` - The part to transfer from.
+                /// * `to_part` - The part to transfer to.
+                /// * `resource` - The name of the resource to transfer.
+                /// * `max_amount` - The maximum amount of resource to transfer.
+                fn start(from_part: Part, to_part: Part, resource: String, max_amount: f32) -> ResourceTransfer {
+                    Start(from_part, to_part, resource, max_amount)
+                }
+            }
+        }
+    }
 );
 
 remote_type!(
